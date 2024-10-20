80543817d3d5537b6b622ebab4b7b374ea15b09c
sid-sg <sengupta.sid03@gmail.com>
2024-10-20 12:12:07 +0530

    webhook server updated

--- apps/webhook-server/src/index.ts
+++ apps/webhook-server/src/index.ts
@@ -56,6 +56,53 @@ app.post("/hdfc-webhook", async (req, res) => {
     }
 });
 
+app.post("/kotak-webhook", async (req, res) => {
+    const info:infoType = {
+        token: req.body.token,
+        userId: req.body.userId,
+        amount: req.body.amount
+    }
+    //todo: zod validation of info
+    //todo: check if req comes from hdfc bank, use webhook secret
+
+    try {
+        await db.$transaction([
+
+            db.balance.update({
+                where: {
+                    userId: Number(info.userId)
+                },
+                data: {
+                    amount: {
+                        increment: Number(info.amount)
+                    }
+                }
+            }),
+
+            db.onRampTransaction.update({
+                where: {
+                    token: info.token
+                },
+                data: {
+                    status: "Success",
+
+                }
+            })
+
+        ]);
+
+        res.json({
+            message: "successfully processed the webhook"
+        })
+    }
+    catch (e) {
+        console.log(e);
+        res.status(411).json({
+            message: "Error while processing webhook"
+        })
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`Webhook server running on port ${PORT}`);
 });
